b487f75ac68549a3155b3ac082f5a508375110ba
Matt <ninevillage@gmail.com>
2015-03-15 18:57:03 +0100

    fix executable pathes

--- test/executablePath.test.js
+++ test/executablePath.test.js
@@ -12,13 +12,24 @@ describe('Helper: executablePath', function () {
     executablePath().should.be.an.instanceOf(String);
 	});
 
-  it('should return the Windows path', function() {
+  it('should return the Windows 32Bit path', function() {
     osMock.platform = sinon.stub().returns('win32');
+		osMock.arch = sinon.stub().returns('ia32');
     executablePath.__set__({
         'os': osMock
     });
 
-    executablePath().should.be.eql(PATHES.WINDOWS);
+    executablePath().should.be.eql(PATHES.EXECUTABLE.WINDOWS.V_32);
+  });
+
+	it('should return the Windows 64Bit path', function() {
+    osMock.platform = sinon.stub().returns('win32');
+		osMock.arch = sinon.stub().returns('x64');
+    executablePath.__set__({
+        'os': osMock
+    });
+
+    executablePath().should.be.eql(PATHES.EXECUTABLE.WINDOWS.V_64);
   });
 
   it('should return the Linux path', function() {
@@ -27,7 +38,7 @@ describe('Helper: executablePath', function () {
         'os': osMock
     });
 
-    executablePath().should.be.eql(PATHES.LINUX);
+    executablePath().should.be.eql(PATHES.EXECUTABLE.LINUX);
   });
 
   it('should return the OSX path', function() {
@@ -36,6 +47,6 @@ describe('Helper: executablePath', function () {
         'os': osMock
     });
 
-    executablePath().should.be.eql(PATHES.OSX);
+    executablePath().should.be.eql(PATHES.EXECUTABLE.OSX);
   });
 });
