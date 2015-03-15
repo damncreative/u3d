115fefe4fc85340d021311e1f35256e2bedf4a49
Matt <ninevillage@gmail.com>
2015-03-15 20:03:23 +0100

    added test for standard asset path

--- test/executablePath.test.js
+++ test/executablePath.test.js
@@ -8,45 +8,91 @@ var osMock = {};
 
 describe('Helper: executablePath', function () {
 
-	it('should return a String', function () {
-    executablePath.Unity().should.be.an.instanceOf(String);
+	describe('For Unity', function() {
+		it('should return a String', function () {
+	    executablePath.Unity().should.be.an.instanceOf(String);
+		});
+
+	  it('should return the Windows 32Bit path', function() {
+	    osMock.platform = sinon.stub().returns('win32');
+			osMock.arch = sinon.stub().returns('ia32');
+	    executablePath.__set__({
+	        'os': osMock
+	    });
+
+	    executablePath.Unity().should.be.eql(PATHES.EXECUTABLE.WINDOWS.V_32);
+	  });
+
+		it('should return the Windows 64Bit path', function() {
+	    osMock.platform = sinon.stub().returns('win32');
+			osMock.arch = sinon.stub().returns('x64');
+	    executablePath.__set__({
+	        'os': osMock
+	    });
+
+	    executablePath.Unity().should.be.eql(PATHES.EXECUTABLE.WINDOWS.V_64);
+	  });
+
+	  it('should return the Linux path', function() {
+	    osMock.platform = sinon.stub().returns('linux');
+	    executablePath.__set__({
+	        'os': osMock
+	    });
+
+	    executablePath.Unity().should.be.eql(PATHES.EXECUTABLE.LINUX);
+	  });
+
+	  it('should return the OSX path', function() {
+	    osMock.platform = sinon.stub().returns('darwin');
+	    executablePath.__set__({
+	        'os': osMock
+	    });
+
+	    executablePath.Unity().should.be.eql(PATHES.EXECUTABLE.OSX);
+	  });
 	});
 
-  it('should return the Windows 32Bit path', function() {
-    osMock.platform = sinon.stub().returns('win32');
-		osMock.arch = sinon.stub().returns('ia32');
-    executablePath.__set__({
-        'os': osMock
-    });
+	describe('For Standard Assets', function() {
+		it('should return a String', function () {
+	    executablePath.StandardAssets().should.be.an.instanceOf(String);
+		});
 
-    executablePath.Unity().should.be.eql(PATHES.EXECUTABLE.WINDOWS.V_32);
-  });
+	  it('should return the Windows 32Bit path', function() {
+	    osMock.platform = sinon.stub().returns('win32');
+			osMock.arch = sinon.stub().returns('ia32');
+	    executablePath.__set__({
+	        'os': osMock
+	    });
 
-	it('should return the Windows 64Bit path', function() {
-    osMock.platform = sinon.stub().returns('win32');
-		osMock.arch = sinon.stub().returns('x64');
-    executablePath.__set__({
-        'os': osMock
-    });
+	    executablePath.StandardAssets().should.be.eql(PATHES.STANDARDASSETS.WINDOWS.V_32);
+	  });
 
-    executablePath.Unity().should.be.eql(PATHES.EXECUTABLE.WINDOWS.V_64);
-  });
+		it('should return the Windows 64Bit path', function() {
+	    osMock.platform = sinon.stub().returns('win32');
+			osMock.arch = sinon.stub().returns('x64');
+	    executablePath.__set__({
+	        'os': osMock
+	    });
 
-  it('should return the Linux path', function() {
-    osMock.platform = sinon.stub().returns('linux');
-    executablePath.__set__({
-        'os': osMock
-    });
+	    executablePath.StandardAssets().should.be.eql(PATHES.STANDARDASSETS.WINDOWS.V_64);
+	  });
 
-    executablePath.Unity().should.be.eql(PATHES.EXECUTABLE.LINUX);
-  });
+	  it('should return the Linux path', function() {
+	    osMock.platform = sinon.stub().returns('linux');
+	    executablePath.__set__({
+	        'os': osMock
+	    });
 
-  it('should return the OSX path', function() {
-    osMock.platform = sinon.stub().returns('darwin');
-    executablePath.__set__({
-        'os': osMock
-    });
+	    executablePath.StandardAssets().should.be.eql(PATHES.STANDARDASSETS.LINUX);
+	  });
 
-    executablePath.Unity().should.be.eql(PATHES.EXECUTABLE.OSX);
-  });
+	  it('should return the OSX path', function() {
+	    osMock.platform = sinon.stub().returns('darwin');
+	    executablePath.__set__({
+	        'os': osMock
+	    });
+
+	    executablePath.StandardAssets().should.be.eql(PATHES.STANDARDASSETS.OSX);
+	  });
+	});
 });
